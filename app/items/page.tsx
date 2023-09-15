0bdf7e74b9d864569ff83f9d004c30f4b6ca0ab2
ShawnPotter <37090129+ShawnPotter@users.noreply.github.com>
2023-09-15 13:53:37 -0700

    created page for display of item ids

--- app/items/page.tsx
+++ app/items/page.tsx
@@ -1,7 +1,49 @@
-import React from 'react'
+'use client'
+import { useEffect, useState } from 'react'
+import Table from '@/app/components/Table/Table'
+import Toolbar from '@/app/components/Toolbar/Toolbar'
 
-const page = () => {
-	return <div className='flex justify-center'>page</div>
+export interface Item {
+	name: string
+	id: string
 }
 
-export default page
+async function getTable(tableName: string) {
+	const res = await fetch(`data/items/${tableName}.json`)
+	const data = await res.json()
+	return data
+}
+
+export default function Items() {
+	const [selectedTable, setSelectedTable] = useState('inorganic')
+	const [data, setData] = useState<Item[]>([])
+
+	//handle table selection
+	const handleTableChange = (newTable: string) => {
+		console.log('New Table to be loaded:', newTable)
+		setSelectedTable(newTable)
+	}
+
+	//fetch the selectedTable data from api
+	useEffect(() => {
+		getTable(selectedTable).then((data) => {
+			setData(data)
+		})
+	}, [selectedTable])
+
+	return (
+		<main className='flex justify-center w-[95%] h-screen'>
+			<div className='flex w-full'>
+				<div className='w-[20%]'>
+					<Toolbar onTableChange={handleTableChange} />
+				</div>
+				<div className='w-[80%]'>
+					<Table
+						tableData={data}
+						tableName={selectedTable}
+					/>
+				</div>
+			</div>
+		</main>
+	)
+}
