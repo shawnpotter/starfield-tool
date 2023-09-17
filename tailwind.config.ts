6c963fa003c971beb94cadd307357ec281f9bdd1
ShawnPotter <37090129+ShawnPotter@users.noreply.github.com>
2023-09-17 16:08:55 -0700

    added two entries backgroundImage

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,19 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'topo-light': "url('../public/background4k_light.png')",
+				'topo-dark': "url('../public/background4k_dark.png')",
+			},
+		},
+	},
+	plugins: [],
 }
 export default config
