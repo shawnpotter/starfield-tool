795d520c2dc8767e8c12015e1a25388ab8da7897
ShawnPotter <37090129+ShawnPotter@users.noreply.github.com>
2023-09-17 16:11:05 -0700

    updated bg and fixed screen height

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			<body className={`${barlow.className} min-h-screen`}>
+			<body className={`${barlow.className} h-screen bg-topo-dark`}>
 				<Navbar />
 				{children}
 			</body>
