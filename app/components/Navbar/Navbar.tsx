096b2a7d5e72c34bf9cc46e71e951fec8252c8a4
ShawnPotter <37090129+ShawnPotter@users.noreply.github.com>
2023-09-20 17:20:39 -0700

    updated navbar to be a block element

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -3,29 +3,31 @@ import React from 'react'
 
 export default function Navbar() {
 	return (
-		<nav className='flex justify-between w-full bg-zinc-800/75 px-10 h-14 items-center'>
-			<div className=''>
-				<Link href='/'>
-					<span>
-						<strong className='text-lg uppercase'>Starfield Tools</strong>{' '}
-						<em>
-							<small>by @Spotter_Dev</small>
-						</em>
-					</span>
-				</Link>
-			</div>
-			<div>
-				<ul className='flex flex-row gap-4'>
-					<li>
-						<Link href='/'>Home</Link>
-					</li>
-					<li>
-						<Link href='/items'>Item IDs</Link>
-					</li>
-					<li>
-						<Link href='/outpost-calculator'>Outpost Calculator</Link>
-					</li>
-				</ul>
+		<nav className='block justify-between w-full bg-zinc-800/75 px-10 h-[56px]'>
+			<div className='flex justify-between items-center h-full'>
+				<div>
+					<Link href='/'>
+						<span>
+							<strong className='text-lg uppercase'>Starfield Tools</strong>{' '}
+							<em>
+								<small>by @Spotter_Dev</small>
+							</em>
+						</span>
+					</Link>
+				</div>
+				<div>
+					<ul className='flex flex-row gap-4'>
+						<li>
+							<Link href='/'>Home</Link>
+						</li>
+						<li>
+							<Link href='/items'>Item IDs</Link>
+						</li>
+						<li>
+							<Link href='/outpost-calculator'>Outpost Calculator</Link>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</nav>
 	)
