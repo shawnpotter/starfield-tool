e10502ac12043096ef2cf8d8eb4411e36b8e4056
ShawnPotter <37090129+shawnpotter@users.noreply.github.com>
2024-06-28 17:42:12 -0700

    added a loading indicator for the updates feed just to smooth any loading

--- app/components/Updates.tsx
+++ app/components/Updates.tsx
@@ -43,20 +43,30 @@ export default function Updates() {
 			)
 		}
 	}
-	return (
-		<div className='flex flex-col justify-start xl:w-1/2 md:px-10 pt-4 md:pt-[12rem] font-semibold text-lg'>
-			<h2>Updates</h2>
-			<div className='flex flex-col border rounded bg-black/50 p-4'>
-				{updates.map((update) => (
-					<div
-						key={update.date}
-						className='mb-2 font-normal'
-					>
-						<p>{update.date}</p>
-						{renderMessage(update.message)}
-					</div>
-				))}
+	if (!updates || updates.length === 0) {
+		return (
+			<div className='flex h-96 justify-center flex-col items-center space-y-2'>
+				<span>Loading Update Feed</span>
+
+				<div className='mt-2 w-6 h-6 border-4 border-gray-200 border-t-blue-500 rounded-full animate-spin'></div>
 			</div>
-		</div>
-	)
+		)
+	} else {
+		return (
+			<div className='flex flex-col justify-start xl:w-1/2 md:px-10 pt-4 md:pt-[12rem] font-semibold text-lg'>
+				<h2>Updates</h2>
+				<div className='flex flex-col border rounded bg-black/50 p-4'>
+					{updates.map((update) => (
+						<div
+							key={update.date}
+							className='mb-2 font-normal'
+						>
+							<p>{update.date}</p>
+							{renderMessage(update.message)}
+						</div>
+					))}
+				</div>
+			</div>
+		)
+	}
 }
