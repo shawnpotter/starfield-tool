70f0a8392ac6c4d850ceaf950ad0794b4344f08b
ShawnPotter <37090129+shawnpotter@users.noreply.github.com>
2024-07-19 13:30:31 -0700

    rearranged component's layout

--- app/components/calculator/TotalMaterialCosts.tsx
+++ app/components/calculator/TotalMaterialCosts.tsx
@@ -8,26 +8,31 @@ const TotalMaterialCosts: React.FC<TotalMaterialCostsProps> = ({
 	totalMaterialCosts,
 }) => {
 	return (
-		<div className='w-full lg:w-3/5 h-full lg:mt-10 justify-start bg-neutral-900/50 overflow-x-hidden lg:mx-10'>
-			<table className='w-full lg:mx-10'>
-				<thead>
-					<tr className='text-left text-lg border-b border-white/75'>
-						<th className='pb-2'>Resource</th>
-						<th className='pb-2'>Amount</th>
-					</tr>
-				</thead>
-				<tbody>
-					{Object.entries(totalMaterialCosts).map(([material, cost]) => (
-						<tr
-							className='border-b border-white/25'
-							key={material}
-						>
-							<td className='capitalize pl-2 mb-2'>{material}</td>
-							<td className='pl-2 mb-2'>{cost}</td>
+		<div className='w-full h-full bg-neutral-900/50 flex flex-col'>
+			<div>
+				<h1 className='w-full'>Material Cost</h1>
+			</div>
+			<div>
+				<table className='table-auto w-[95%]'>
+					<thead>
+						<tr className='text-lg text-left border-b border-white/75'>
+							<th className='pb-2'>Resource</th>
+							<th className='pb-2'>Amount</th>
 						</tr>
-					))}
-				</tbody>
-			</table>
+					</thead>
+					<tbody>
+						{Object.entries(totalMaterialCosts).map(([material, cost]) => (
+							<tr
+								className='border-b border-white/25'
+								key={material}
+							>
+								<td className='capitalize'>{material}</td>
+								<td className=''>{cost}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
 		</div>
 	)
 }
