88e838fb19eeb6ad7ff324f877328fa95b4efb17
Shawn Potter <potter.shawn.donovan@gmail.com>
2024-08-07 11:54:54 -0700

    updated layout

--- app/components/calculator/TotalMaterialCosts.tsx
+++ app/components/calculator/TotalMaterialCosts.tsx
@@ -8,14 +8,16 @@ const TotalMaterialCosts: React.FC<TotalMaterialCostsProps> = ({
 	totalMaterialCosts,
 }) => {
 	return (
-		<div className='w-full h-full bg-neutral-900/50 flex flex-col'>
+		<div className='w-full mt-4 flex flex-col'>
 			<div>
-				<h1 className='w-full'>Material Cost</h1>
+				<h1 className='w-full text-base font-semibold mb-[1.25rem]'>
+					Material Cost
+				</h1>
 			</div>
 			<div>
-				<table className='table-auto w-[95%]'>
+				<table className='table-auto w-full'>
 					<thead>
-						<tr className='text-lg text-left border-b border-white/75'>
+						<tr className='text-left border-b-2 border-dashed'>
 							<th className='pb-2'>Resource</th>
 							<th className='pb-2'>Amount</th>
 						</tr>
@@ -23,10 +25,10 @@ const TotalMaterialCosts: React.FC<TotalMaterialCostsProps> = ({
 					<tbody>
 						{Object.entries(totalMaterialCosts).map(([material, cost]) => (
 							<tr
-								className='border-b border-white/25'
+								className='border-b'
 								key={material}
 							>
-								<td className='capitalize'>{material}</td>
+								<td className='capitalize p-0.5'>{material}</td>
 								<td className=''>{cost}</td>
 							</tr>
 						))}
