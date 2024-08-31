2c0db4b08b369fa15083dedaa5b2c36340851a02
Shawn Potter <37090129+shawnpotter@users.noreply.github.com>
2024-08-31 13:23:22 -0700

    added exportable interfaces for types

---
+++ utils/weaponType.ts
@@ -0,0 +1,38 @@
+//weaponType.ts
+export interface Mod {
+	name: string
+	description: string
+	accuracyModifier?: number
+	damageModifier?: number
+	massModifier?: number
+	magazineCapacityModifier?: number
+	fireRateModifier?: number
+	rangeModifier?: number
+	reloadSpeedModifier?: number
+	stabilityModifier?: number
+	valueModifier?: number
+}
+
+export interface ModSlot {
+	slotType: string
+	mods: Mod[]
+	defaultMod?: number // Index of the default mod in the mods array
+	disabled?: boolean
+}
+
+export interface Weapon {
+	name: string
+	category: string
+	damageType: string
+	damageType2?: string
+	damage: number
+	damage2?: number
+	ammoType?: string
+	magCapacity?: number
+	fireRate?: number
+	range?: number
+	accuracy?: number
+	mass: number
+	value: number
+	modSlots: ModSlot[]
+}
