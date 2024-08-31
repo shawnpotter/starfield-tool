b51c726b95d3f30f54fa959c96701dadf05c0901
Shawn Potter <37090129+shawnpotter@users.noreply.github.com>
2024-08-31 13:24:52 -0700

    made index page for easier export of weapon categories

---
+++ utils/weapons/index.ts
@@ -0,0 +1,8 @@
+import { melee } from './melee'
+import { rifles } from './rifles'
+import { pistols } from './pistols'
+import { shotguns } from './shotguns'
+import { heavy } from './heavy'
+import { energy } from './energy'
+
+export { melee, rifles, pistols, shotguns, heavy, energy }
